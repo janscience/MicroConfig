f2810f1d785ea4c5b1e8eb72b1e456c602829e70
Jan Benda <jan.benda@uni-tuebingen.de>
2026-08-15 23:13:41 +0200

    [Storage] added Storage.cpp

---
+++ src/Storage.cpp
@@ -0,0 +1,52 @@
+#include <EEPROM.h>
+#include <Storage.h>
+
+
+Storage::Storage() {
+}
+
+
+uint16_t Storage::length() {
+  return EEPROM.length();
+}
+
+
+void Storage::read(int idx, uint8_t *dest, size_t len) {
+  while (len--)
+    *dest++ = EEPROM.read(idx++);
+}
+
+  
+void Storage::update(int idx, const uint8_t *src, size_t len) {
+  while (len--)
+    EEPROM.update(idx++, *src++);
+}
+
+
+uint32_t Storage::crc(int addr0, int addr1) {
+  // Adapted from https://docs.arduino.cc/learn/programming/eeprom-guide/
+  
+  const uint32_t crc_table[16] = {
+    0x00000000, 0x1db71064, 0x3b6e20c8, 0x26d930ac,
+    0x76dc4190, 0x6b6b51f4, 0x4db26158, 0x5005713c,
+    0xedb88320, 0xf00f9344, 0xd6d6a3e8, 0xcb61b38c,
+    0x9b64c2b0, 0x86d3d2d4, 0xa00ae278, 0xbdbdf21c
+  };
+
+  if (addr0 < 0)
+    addr0 = 0;
+  if (addr1 > length())
+    addr1 = length();
+  uint8_t buffer[addr1 - addr0];
+  read(addr0, buffer, addr1 - addr0);
+  uint8_t val = 0;
+  uint32_t crc = ~0L;
+  for (int index = 0; index < addr1 - addr0; ++index) {
+    val = buffer[index];
+    crc = crc_table[(crc ^ val) & 0x0f] ^ (crc >> 4);
+    crc = crc_table[(crc ^ (val >> 4)) & 0x0f] ^ (crc >> 4);
+    crc = ~crc;
+  }
+  return crc;
+}
+
